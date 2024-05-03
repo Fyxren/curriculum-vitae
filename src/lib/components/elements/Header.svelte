c92e8df67b64f20244f469fc7bde4cb5d932a9a2
Fyxren <benweerd@gmail.com>
2024-05-03 12:08:15 +0200

    fix: header link target & hover effect

--- src/lib/components/elements/Header.svelte
+++ src/lib/components/elements/Header.svelte
@@ -3,5 +3,9 @@
 
 <div class="flex flex-col px-40 py-52">
 	<h1 class="font-bold text-7xl">Ben van der Weerd</h1>
-	<a href="https://graafschapcollege.nl/profielen/technicus-engineering/" class="mt-2 text-xl">Smart Industry student @ Graafschap College</a>
+	<a
+		href="https://graafschapcollege.nl/profielen/technicus-engineering/"
+		class="mt-2 text-xl hover-link"
+		target="_blank">Smart Industry student @ Graafschap College</a
+	>
 </div>
