3afbb3cce496a0433e1829972c85bd4068d4edbe
Fyxren <benweerd@gmail.com>
2024-05-19 17:46:33 +0200

    feat: add header image

---
+++ src/lib/components/atoms/HeaderImage.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let rotation = 0;
+
+	function handleMouseMove(event: MouseEvent) {
+		const target = event.currentTarget as HTMLDivElement;
+		const rect = target.getBoundingClientRect();
+		const x = event.clientX - rect.left - rect.width / 2;
+		rotation = (x / (rect.width / 2)) * 5;
+		console.log('ROTATION = ', rotation);
+	}
+
+	function handleMouseLeave(event: MouseEvent) {
+		rotation = 0;
+	}
+</script>
+
+<div
+	class="w-64 h-64 image-hover"
+	on:mousemove={handleMouseMove}
+	on:mouseleave={handleMouseLeave}
+	role="none"
+>
+	<img
+		src="/cv_img.png"
+		alt="My face"
+		class="rounded-full opacity-50 contrast-125 grayscale hover:scale-110 hover:opacity-100 hover:contrast-100 hover:grayscale-0"
+		style="transform: rotate({rotation}deg); transition: transform 0.2s linear;"
+	/>
+</div>
--- src/lib/components/elements/Header.svelte
+++ src/lib/components/elements/Header.svelte
@@ -1,11 +1,16 @@
 <script>
+	import HeaderImage from "../atoms/HeaderImage.svelte";
+
 </script>
 
-<div class="flex flex-col px-40 py-52">
-	<h1 class="font-bold text-7xl">Ben van der Weerd</h1>
-	<a
-		href="https://graafschapcollege.nl/profielen/technicus-engineering/"
-		class="mt-2 text-xl hover-link"
-		target="_blank">Smart Industry student @ Graafschap College</a
-	>
+<div class="flex items-center justify-between px-40 py-40">
+	<div class="flex flex-col">
+		<h1 class="font-bold text-7xl">Ben van der Weerd</h1>
+		<a
+			href="https://graafschapcollege.nl/profielen/technicus-engineering/"
+			class="mt-2 text-xl hover-link"
+			target="_blank">Smart Industry student @ Graafschap College</a
+		>
+	</div>
+	<HeaderImage />
 </div>
